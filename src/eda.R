94a5c14f1ddc71b3f61dd68926802a4076426e3b
adalisan <sadali@gmail.com>
2017-06-24 01:25:54 -0400

    generate omnibus dissim matrix

--- src/eda.R
+++ src/eda.R
@@ -1,8 +1,23 @@
 library('ProjectTemplate')
 load.project()
 
-for (dataset in project.info$data)
-{
-  message(paste('Showing top 5 rows of', dataset))
-  print(head(get(dataset)))
+source("./src/real.data.experiment.fn.R")
+download.file("http://cancerres.aacrjournals.org/highwire/filestream/335671/field_highwire_adjunct_files/2/177323_1_supp_3899876_bmkvbb.xlsx", destfile = "data/simmatrix.xlsx")
+
+matlist = list()
+entities = list()
+for ( sh in 1:4 ) {
+  sim.mat <- readxl::read_excel("data/simmatrix.xlsx", sheet = sh )
+  rownames(sim.mat) <- as.vector(t(sim.mat[,1]))
+  sim.mat <- sim.mat[,-1]
+  entities <- union(colnames(sim.mat), entities)
+  sim.mat <- as.matrix(sim.mat)
+  
+  matlist = c( matlist, list(sim.mat))
 }
+init_mat <- array(0,dim = c(9,dim(matlist[[1]])[1], dim(matlist[[1]])[1]))
+for (k in 1:9){
+  diag(init_mat[k, , ]) = 1
+  print(head(init_mat[k, ,]))
+}
+omni.mat<-OmnibusEmbed::omnibusM.Kcond(matlist[1:4],init_mat)
