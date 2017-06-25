c1708d0a1949f931f829770a250d10fa91768eeb
adalisan <sadali@gmail.com>
2017-06-25 00:46:21 -0400

    add jacknife experiment function

--- src/eda.R
+++ src/eda.R
@@ -20,4 +20,46 @@ for (k in 1:9){
   diag(init_mat[k, , ]) = 1
   print(head(init_mat[k, ,]))
 }
-omni.mat<-OmnibusEmbed::omnibusM.Kcond(matlist[1:4],init_mat)
+init_mat <- array(0,dim = c(1,dim(matlist[[1]])[1], dim(matlist[[1]])[1]))
+
+diag(init_mat[1, , ]) = 1
+#  print(head(init_mat[1, ,]))
+library(doSNOW)
+
+
+cl <- parallel::makeCluster(8) #change the 2 to your number of CPU cores
+nmc = 8
+
+registerDoSNOW(cl)
+for (i in 1:3)
+  for (j in 2:4){
+    omni.mat <- OmnibusEmbed::omnibusM.Kcond(matlist[c(i,j)],init_mat)
+    w.val.vec <- 0.8
+    rep.i <- 1
+    size.vec <- seq(0, 1, 0.05)
+    first_result <- OmnibusEmbed::run.JOFC.match.jacknife.replicate (m.i = rep.i, N = 239, test.samp.size = 20, 
+                                                     w.val.len = 1, 
+                                                     Diss.E = matlist[[i]], Diss.F = matlist[[j]], 
+                                                     d = 3,   oos = TRUE,   separability.entries.w = TRUE
+                                                     , wt.equalize = FALSE, assume.matched.for.oos = TRUE
+                                                     , oos.use.imputed = FALSE, 
+                                                     w.vals = w.val.vec, size = size.vec, 
+                                                     verbose = TRUE, level.critical.val = 0.05)
+    jk.res.IsoJ <- foreach (rep.i = 1:nmc) %dopar% {
+    
+
+      
+      
+    res[[rep.i]]  <- OmnibusEmbed::run.JOFC.match.jacknife.replicate (m.i = rep.i, N = 239, test.samp.size = 20, 
+                                         w.val.len = 1, 
+                                         Diss.E = matlist[[i]], Diss.F = matlist[[j]], 
+                                         d = 3,   oos = TRUE,   separability.entries.w = FALSE
+                                         , wt.equalize = FALSE, assume.matched.for.oos = TRUE
+                                         , oos.use.imputed = FALSE, 
+                                         w.vals = w.val.vec, size = size.vec, 
+                                         verbose = TRUE, level.critical.val = 0.05)
+
+  }
+  
+    
+}
