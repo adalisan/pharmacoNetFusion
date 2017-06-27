49180304219bbe4962d6004a0bbddb1fee702531
adalisan <sadali@gmail.com>
2017-06-27 02:12:07 -0400

    changes

--- src/eda.R
+++ src/eda.R
@@ -4,6 +4,7 @@ load.project()
 source("./src/real.data.experiment.fn.R")
 download.file("http://cancerres.aacrjournals.org/highwire/filestream/335671/field_highwire_adjunct_files/2/177323_1_supp_3899876_bmkvbb.xlsx", destfile = "data/simmatrix.xlsx")
 
+
 matlist = list()
 entities = list()
 for ( sh in 1:4 ) {
@@ -12,9 +13,15 @@ for ( sh in 1:4 ) {
   sim.mat <- sim.mat[,-1]
   entities <- union(colnames(sim.mat), entities)
   sim.mat <- as.matrix(sim.mat)
-  
+  #sim.mat <- 1- sim.mat
   matlist = c( matlist, list(sim.mat))
 }
+diss.matlist <- list()
+for (mat in matlist) {
+  mat = mat / max(max(mat))
+  diss.matlist <- c(diss.matlist,list(mat))
+}
+saveRDS(matlist,"data/DissimilarityMatrixList.RData")
 init_mat <- array(0,dim = c(9,dim(matlist[[1]])[1], dim(matlist[[1]])[1]))
 for (k in 1:9){
   diag(init_mat[k, , ]) = 1
@@ -33,7 +40,7 @@ nmc = 8
 registerDoSNOW(cl)
 for (i in 1:3)
   for (j in 2:4){
-    omni.mat <- OmnibusEmbed::omnibusM.Kcond(matlist[c(i,j)],init_mat)
+    omni.mat <- OmnibusEmbed::omnibusM.Kcond(diss.matlist[c(i,j)],init_mat)
     w.val.vec <- 0.8
     rep.i <- 1
     size.vec <- seq(0, 1, 0.05)
@@ -45,6 +52,8 @@ for (i in 1:3)
                                                      , oos.use.imputed = FALSE, 
                                                      w.vals = w.val.vec, size = size.vec, 
                                                      verbose = TRUE, level.critical.val = 0.05)
+  }
+1
     jk.res.IsoJ <- foreach (rep.i = 1:nmc) %dopar% {
     
 
